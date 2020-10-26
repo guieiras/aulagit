0f0a4d2da8dcdffcff9cc1e8ac7d6ac8e72daba5
Guilherme Eiras <guilherme.eiras@revelo.com.br>
2020-10-26 19:21:09 -0300

    adicionando bem-vindo

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function Oi() {
 function Tchau() {
   return 'Tchau'
 }
+
+function BemVindoPara(pessoa) {
+  return `${OiParaPessoa(pessoa)}. Que bom que você está de volta no nosso site incrível!!!`
+}
